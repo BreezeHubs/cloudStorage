a496d8e1bd6c7a61c70cde76a310864b52006708
iuR <2356665641@qq.com>
2022-08-05 18:06:39 +0800

    1

--- config/oss.go
+++ config/oss.go
@@ -4,5 +4,5 @@ const (
 	OSS_BUCKET_NAME   = "i026-cloud-storage"
 	OSS_ENDPOINT      = "oss-cn-guangzhou.aliyuncs.com"
 	OSS_ACCESS_KEY    = "LTAI5tDjrL9nn7Qq4o8ZZ9yP"
-	OSS_ACCESS_SECRET = "lnD74drqEtO2PddIwNYXZYL40qfcUI"
+	OSS_ACCESS_SECRET = "lnD74drqEtO2PddIwNYXZYL40qfcUIIURZZ"
 )
